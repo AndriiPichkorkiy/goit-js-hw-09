2d7f2994c70defede69539061fc81702459bb2fe
Andrey <93935023+desertptu4ka@users.noreply.github.com>
2022-06-06 12:00:56 +0300

    test

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,7 +5,7 @@ const form = document.querySelector('.form');
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
+    // setTimeout(() => {
       if (shouldResolve) {
         // Fulfill
         resolve({ position, delay });
@@ -13,34 +13,68 @@ function createPromise(position, delay) {
         // Reject
         reject({ position, delay });
       }
-    }, delay);
+    // }, delay);
   });
 }
 
 function createPromises() {
   event.preventDefault();
-  const {
-    delay: { value: delay },
-    step: { value: step },
-    amount: { value: amount },
+  let {
+    delay: { valueAsNumber: delay },
+    step: { valueAsNumber: step },
+    amount: { valueAsNumber: amount },
   } = form;
 
   //check if values === '' or 0
-  if (!+delay || !+step || !+amount) {
+  if (!delay || !step || !amount) {
     Notify.warning('pls fill in all filds rigth');
     return;
   }
 
-  for (let i = 0; i < +amount; i += 1) {
-    const stepDelay = +delay + +step * i;
-    createPromise(i + 1, stepDelay)
-      .then(({ position, delay }) => {
-        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
-      });
+  // for (let i = 0; i < amount; i += 1) {
+  //   const stepDelay = delay + step * i;
+  //   createPromise(i + 1, stepDelay)
+  //     .then(({ position, delay }) => {
+  //       Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+  //     })
+  //     .catch(({ position, delay }) => {
+  //       Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+  //     });
+  // }
+  const formData = {
+    delay,
+    step,
+    amount,
+    currentNumberOfPromise: 0,
   }
+  function doOnce(delay) {
+    console.log('do once');
+    doPromise(delay);
+    const idForInterval = setInterval(() => {
+      console.log('Interval', step);
+      if (formData.currentNumberOfPromise >= formData.amount) return clearInterval(idForInterval);
+      doPromise();
+    }, step);
+  }
+  
+  function doPromise() {
+    createPromise(
+      formData.currentNumberOfPromise + 1,
+      formData.step * formData.currentNumberOfPromise,
+    )
+      .then(onPromiseSuccess)
+      .catch(onPromiseError);
+    formData.currentNumberOfPromise += 1;
+  }
+  setTimeout(doOnce, delay);
+}
+
+function onPromiseSuccess({ position, delay }) {
+  Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function onPromiseError({ position, delay }) {
+  Notify.failure(`Rejected promise ${position} in ${delay}ms`);
 }
 
 form.addEventListener('submit', createPromises);
